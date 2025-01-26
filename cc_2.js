7e0504f337116ac246712351bfe34d530773984e
Linh Pham <linhmaipham@usf.edu>
2025-01-26 17:08:47 -0500

    Task 3 - Employee Records

--- cc_2.js
+++ cc_2.js
@@ -9,3 +9,15 @@ let scores = [1,2,3,4,5];
 scores[1] = 6;
 let average = scores.reduce((sum,score) => sum+score,0)/scores.length;
 console.log(`Updated array: ${scores} - Average score: ${average}`);
+
+//Task 3: Working with Objects
+//Submission: Commit your changes to GitHub with the message: "Task 3 - Employee Records."
+let employee = {
+    name: "Linh Pham",
+    age: 19,
+    department: "Finance",
+    isActive: true
+};
+employee.department = "HR";
+employee.position = "HR Assistant";
+console.log(employee);
