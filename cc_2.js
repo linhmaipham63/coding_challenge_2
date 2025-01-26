25c12a32e617a25d7fc16035e7f59e81497eb4fb
Linh Pham <linhmaipham@usf.edu>
2025-01-26 17:06:57 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -3,3 +3,9 @@ let products = ["Banh mi","Pho","Bun cha","Bun bo Hue","Bun rieu","Com tam"];
 products.unshift("Banh cuon");
 products.pop();
 console.log(products);
+
+//Task 2: Accessing and Modifying Arrays
+let scores = [1,2,3,4,5];
+scores[1] = 6;
+let average = scores.reduce((sum,score) => sum+score,0)/scores.length;
+console.log(`Updated array: ${scores} - Average score: ${average}`);
